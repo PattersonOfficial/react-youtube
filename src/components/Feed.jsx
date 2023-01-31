c1045a948263f64ccf669ea03a55aa0afdb0ce63
Benjamin Akrong <akrongwork@gmail.com>
2023-01-31 10:27:49 +0000

    sidebar typography complete

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
 
-export default Feed
+  const date = new Date().getFullYear();
+  return (
+    // Main Feed Container
+    <Stack sx={{ flexDirection: { sm: 'column', md: 'row' } }}>
+      
+      {/* Main Feed Content */}
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}>
+
+          {/* SideBar Component */}
+          SideBar
+
+
+          {/* Copyright Component */}
+          <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+            Copyright ©{date} Asher Technologies
+          </Typography>
+
+        </Box>
+
+    </Stack>
+  );
+};
+
+export default Feed;
