41e8f8ce1bdcfc070421b7d43b9784a0e9339492
Benjamin Akrong <akrongwork@gmail.com>
2023-02-01 17:43:56 +0000

    sidebar component styling complete

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import SideBar from './minor-components/SideBar';
 
 const Feed = () => {
-
   const date = new Date().getFullYear();
   return (
     // Main Feed Container
-    <Stack sx={{ flexDirection: { sm: 'column', md: 'row' } }}>
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
       
-      {/* Main Feed Content */}
+      {/* Sidebar Container */}
       <Box
         sx={{
           height: { sx: 'auto', md: '92vh' },
@@ -16,16 +16,29 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}>
 
-          {/* SideBar Component */}
-          SideBar
+
+        {/* SideBar Component */}
+        <SideBar />
 
 
-          {/* Copyright Component */}
-          <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
-            Copyright ©{date} Asher Technologies
-          </Typography>
+        {/* Copyright Component */}
+        <Typography
+          className='copyright'
+          variant='body2'
+          sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright ©{date} Asher Technologies
+        </Typography>
+      </Box>
 
-        </Box>
+      {/* Main Feed Container */}
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+
+        {/* Content Heading Component */}
+        <Typography variant='h4' fontWeight='bold'  mb={2} sx={{ color: 'white' }}>
+          New <span style={{ color: '#FC1503' }} >Videos</span>
+        </Typography>
+
+      </Box>
 
     </Stack>
   );
--- src/components/minor-components/SideBar.jsx
+++ src/components/minor-components/SideBar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Stack } from '@mui/system';
+import { categories } from '../../utils/constants';
 
-const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
-}
+const selectedCategory = 'New';
 
-export default SideBar
+const SideBar = () => (
+  <Stack
+    direction='row'
+    sx={{
+      overflowY: 'auto',
+      height: {
+        sx: 'auto',
+        md: '95%',
+      },
+      flexDirection: {
+        md: 'column',
+      },
+    }}>
+    {categories.map((category) => (
+      <button
+        className='category-btn'
+        key={category.name}
+        style={{
+          color: 'white',
+          background: category.name === selectedCategory && '#FC1503',
+        }}>
+        <span
+          style={{
+            color: category.name === selectedCategory ? 'white' : 'red',
+            marginRight: '15px',
+          }}>
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default SideBar;
