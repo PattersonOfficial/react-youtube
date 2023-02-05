ead3b838795ff9a0d9e7dcbbcc1e2db29f1aa898
Benjamin Akrong <akrongwork@gmail.com>
2023-02-05 19:58:19 +0000

    Search functionality and search page complete

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import Videos from './Videos';
+import { fetchFromAPI } from '../utils/service';
+import { useParams } from 'react-router-dom';
 
-const SearchFeed = () => {
+const Feed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setVideos(data?.items);
+    });
+  }, [searchTerm]);
+
   return (
-    <div>SearchFeed</div>
-  )
-}
+    // Main Search  Container
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      {/* Content Heading Component */}
+      <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+        Search Results: <span style={{ color: '#FC1503' }}>{searchTerm}</span>{' '}
+        Videos
+      </Typography>
 
-export default SearchFeed
+      {/* Videos Component */}
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default Feed;
--- src/components/minor-components/SearchBar.jsx
+++ src/components/minor-components/SearchBar.jsx
@@ -3,28 +3,42 @@ import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
-const SearchBar = () => (
-  <Paper
-    component='form'
-    onSubmit={() => {}}
-    sx={{
-      border: '1px solid #e3e3e3',
-      borderRadius: 20,
-      pl: 2,
-      boxShadow: 'none',
-      mr: { sm: 5 },
-    }}>
-    <input
-      className='search-bar'
-      placeholder='Search...'
-      value=''
-      onChange={() => {}}
-    />
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm ] = useState('');
 
-    <IconButton type='submit' sx={{ p: '10', color: 'red' }}>
-      <Search />
-    </IconButton>
-  </Paper>
-);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  }
+
+  return (
+    <Paper
+      component='form'
+      onSubmit={handleSubmit}
+      sx={{
+        border: '1px solid #e3e3e3',
+        borderRadius: 20,
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}>
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <IconButton type='submit' sx={{ p: '10', color: 'red' }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
 
 export default SearchBar;
